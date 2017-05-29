59d775f61254465126adff11294be9c448815c54
Michael Czolko <michael@czolko.cz>
2017-05-29 18:34:22 +0200

    Added Redux

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Boilerplate is done via https://github.com/facebookincubator/create-react-app
 
 Deployment was easily done by help of Heroku https://blog.heroku.com/deploying-react-with-zero-configuration#zero-configuration-deploy-to-heroku
 
-The app is running here: https://lit-plains-89085.herokuapp.com/
+The app is running here: https://lit-plains-89085.herokuapp.com/ (the initial load is sometimes really slow because the dyno needs few seconds to spin up)
+
+State will be handled via Redux https://github.com/reactjs/redux
--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import todoApp from './reducers';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(
+  todoApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // developer happiness
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  , document.getElementById('root'));
 registerServiceWorker();
